e1ba30e0919c4e492bf7b18432236f78f841b269
Jordi <jordi8101@gmail.com>
2023-10-02 18:29:09 -0700

    feat: added multiple selection

--- src/components/ObjectDetector/ObjectDetector.jsx
+++ src/components/ObjectDetector/ObjectDetector.jsx
@@ -5,10 +5,12 @@ import { normalizePredictions } from "../../utils";
 import { TargetBox } from "../TargetBox/TargetBox";
 
 const ObjectDetector = () => {
-  const [image, setImage] = useState(null);
-  const [imageResult, setImageResult] = useState(null);
-  const [error, setError] = useState(null);
+  const [image, setImage] = useState();
+  const [foundedCarPlates, setFoundedCarPlates] = useState([]);
   const [predictions, setPredictions] = useState([]);
+  const [foundedCars, setFoundedCars] = useState([]);
+
+  const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
   const imageRef = useRef(null);
@@ -17,9 +19,20 @@ const ObjectDetector = () => {
     e.preventDefault();
   };
 
+  const resetState = () => {
+    setImage(null);
+    setFoundedCarPlates([]);
+    setPredictions([]);
+    setFoundedCars([]);
+    setError(null);
+    setLoading(false);
+  };
+
   const handleChangeImage = (e) => {
     const file = e.target.files[0];
     const reader = new FileReader();
+
+    resetState();
 
     setImage(file);
 
@@ -27,8 +40,6 @@ const ObjectDetector = () => {
 
     reader.onloadend = async () => {
       setLoading(true);
-      setError(false);
-      setImageResult(null);
 
       const imageElement = document.createElement("img");
       imageElement.src = reader.result;
@@ -40,33 +51,33 @@ const ObjectDetector = () => {
         });
 
         if (predictions.length) {
-          const canvas = document.createElement("canvas");
-          const ctx = canvas.getContext("2d");
+          predictions.forEach((prediction) => {
+            // create a canvas element
+            const canvas = document.createElement("canvas");
+            const ctx = canvas.getContext("2d");
 
-          const x = predictions[0].bbox[0];
-          const y = predictions[0].bbox[1];
-          const width = predictions[0].bbox[2];
-          const height = predictions[0].bbox[3];
+            const x = prediction.bbox[0];
+            const y = prediction.bbox[1];
+            const width = prediction.bbox[2];
+            const height = prediction.bbox[3];
 
-          canvas.width = width;
-          canvas.height = height;
+            canvas.width = width;
+            canvas.height = height;
 
-          ctx.drawImage(imageElement, x, y, width, height, 0, 0, width, height);
+            ctx.drawImage(
+              imageElement,
+              x,
+              y,
+              width,
+              height,
+              0,
+              0,
+              width,
+              height
+            );
 
-          document.body.appendChild(canvas);
-
-          const base64 = canvas.toDataURL("image/png");
-
-          await fetch(process.env.REACT_APP_API_SERVER + "/api/process-image", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ base64 }),
-          })
-            .then((response) => response.json())
-            .then((data) => setImageResult(data.result))
-            .catch(() => setError(true));
+            setFoundedCars((prev) => [...prev, canvas.toDataURL("image/png")]);
+          });
         }
       };
 
@@ -87,6 +98,25 @@ const ObjectDetector = () => {
     setPredictions(normalizedPredictions);
 
     return normalizedPredictions;
+  };
+
+  const handleImageClick = async (e) => {
+    setLoading(true);
+
+    const base64 = e.target.src;
+
+    await fetch(process.env.REACT_APP_API_SERVER + "/api/process-image", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ base64 }),
+    })
+      .then((response) => response.json())
+      .then((data) => setFoundedCarPlates(data.result))
+      .catch(() => setError(true));
+
+    setLoading(false);
   };
 
   return (
@@ -113,11 +143,36 @@ const ObjectDetector = () => {
             ))}
         </div>
       )}
-      {imageResult && (
+      {foundedCars.length > 0 && (
+        <div className="founded_cars">
+          <h2>Founded cars</h2>
+          <span>Click on the car to see the car plate</span>
+          <div className="founded_cars_list">
+            {foundedCars.map((car, idx) => (
+              <img
+                key={idx}
+                src={car}
+                alt=""
+                className="founded_car"
+                onClick={handleImageClick}
+              />
+            ))}
+          </div>
+        </div>
+      )}
+      {foundedCarPlates.length > 0 && (
         <div className="image_result">
-          <h2>Predicted car plate</h2>
-          <img src={`data:image/png;base64,${imageResult}`} alt="" />
-          <span>The value of the car plate is:</span>
+          <h2>Possible founded car plates:</h2>
+          <div className="image_result_list">
+            {foundedCarPlates.map((plate, idx) => (
+              <img
+                key={idx}
+                src={"data:image/png;base64," + plate}
+                alt=""
+                className="image_result_item"
+              />
+            ))}
+          </div>
         </div>
       )}
       {error && <p>Image not processed, please try again</p>}
